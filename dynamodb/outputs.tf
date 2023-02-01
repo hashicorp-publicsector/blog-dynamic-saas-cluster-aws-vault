92ee3da9511e526bcdd60a2120ad0b5ac8f6f34d
Sean Doyle <sean.doyle@hashicorp.com>
2023-01-31 16:42:55 -0800

    Cleaning up files to archive

--- dynamodb/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_dynamodb_table_tfer--Products_xdgssrli_id" {
-  value = "${aws_dynamodb_table.tfer--Products_xdgssrli.id}"
-}
--- igw/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_internet_gateway_tfer--igw-040a85d3eba854b61_id" {
-  value = "${aws_internet_gateway.tfer--igw-040a85d3eba854b61.id}"
-}
-
-output "aws_internet_gateway_tfer--igw-0e6139e7bf31c95b0_id" {
-  value = "${aws_internet_gateway.tfer--igw-0e6139e7bf31c95b0.id}"
-}
