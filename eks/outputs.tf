5d3fe3a3a407f3c6a7118ce3a351d1577e6a47c6
Sean Doyle <sean.doyle@hashicorp.com>
2023-01-31 18:20:23 -0800

    Moving eks folder to archived

--- eks/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_eks_cluster_tfer--dynamic-policy-saas_id" {
-  value = "${aws_eks_cluster.tfer--dynamic-policy-saas.id}"
-}
-
-output "aws_eks_node_group_tfer--nodegroup_id" {
-  value = "${aws_eks_node_group.tfer--nodegroup.id}"
-}
